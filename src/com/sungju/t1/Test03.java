1562bc05262a58536cd080bc0a82caab8d4e2efe
sunju <kimtjden@naver.com>
2019-09-20 10:48:40 +0900

    자바.메스

---
+++ src/com/sungju/t1/Test03.java
@@ -0,0 +1,14 @@
+package com.sungju.t1;
+
+public class Test03 {
+	public static void main(String[] args) {
+		System.out.println(Math.ceil(6.2));
+		System.out.println(Math.floor(6.2));
+		System.out.println(Math.round(6.48));
+		System.out.println("구분선구분");
+		for (int i = 0; i < 10; i++) {
+			System.out.println((int) (Math.random() * 10));
+		}
+	}
+
+}
